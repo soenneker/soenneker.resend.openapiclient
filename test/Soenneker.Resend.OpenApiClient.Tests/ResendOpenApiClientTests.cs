7028d6ed27cbbf4c635bd25c74dcdb853c3db2f6
Jake Soenneker <jake@soenneker.com>
2025-06-03 20:24:00 -0500

    Initial commit

---
+++ test/Soenneker.Resend.OpenApiClient.Tests/ResendOpenApiClientTests.cs
@@ -0,0 +1,21 @@
+﻿using Soenneker.Tests.FixturedUnit;
+using Xunit;
+
+namespace Soenneker.Resend.OpenApiClient.Tests;
+
+[Collection("Collection")]
+public sealed class ResendOpenApiClientTests : FixturedUnitTest
+{
+
+
+    public ResendOpenApiClientTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+
+    }
+
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
